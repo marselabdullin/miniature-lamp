bf331ec71ea3ff4f206be8bb4ace2f8c8cc97174
marselabdullin <abdmars@mail.ru>
2022-10-06 15:02:14 +0300

    Add stylelint & sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
-import './App.css';
+import css from './App.module.scss';
 import logo from './logo.svg';
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={css.App}>
+      <header className={css.AppHeader}>
+        <img src={logo} className={css.AppLogo} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        <a className={css.AppLink} href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
