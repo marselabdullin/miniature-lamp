7da39d2a2fb141c9136064c8920dc381c49fdeca
marselabdullin <abdmars@mail.ru>
2022-10-22 10:27:36 +0300

    Fix createRoot warning

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import './index.scss';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 
 import App from './App';
 
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <App />
